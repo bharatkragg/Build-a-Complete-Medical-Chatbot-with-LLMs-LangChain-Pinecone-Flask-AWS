0189d2f241de306826540762e5d6f27c3a3d98d9
Bharat <bharatagg@yahoo.com>
2026-06-24 01:11:24 +0530

    Folder Structure Added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Making Directory
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+touch src/__int__.py
+touch src/helper.py
+touch src/promt.py
+touch .env
+touch setup.py
+touch research/trials.ipynb
+touch requirment.txt
+
+
+echo "Directory and Files Created Successfully"
